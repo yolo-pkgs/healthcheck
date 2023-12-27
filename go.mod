49328398997ddb8cf66cebc7e140cfa40790e772
Gleb Buzin <qufiwefefwoyn@gmail.com>
2023-12-28 01:37:43 +0300

    feat: init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yolo-pkgs/healthcheck
+
+go 1.20
---
+++ server.go
@@ -0,0 +1,48 @@
+package healthcheck
+
+import (
+	"net/http"
+)
+
+type Server struct {
+	s         *http.Server
+	readyChan <-chan bool
+	ready     bool
+}
+
+func (s *Server) Serve(addr string) {
+	go func() {
+		for ready := range s.readyChan {
+			s.ready = ready
+		}
+	}()
+
+	s.s.ListenAndServe()
+}
+
+func New(addr string, readyChan <-chan bool) *Server {
+	srv := Server{
+		s:         &http.Server{Addr: addr},
+		readyChan: readyChan,
+		ready:     false,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/live", srv.Live)
+	mux.HandleFunc("/ready", srv.Ready)
+	srv.s.Handler = mux
+
+	return &srv
+}
+
+func (s *Server) Live(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (s *Server) Ready(w http.ResponseWriter, r *http.Request) {
+	if s.ready {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}
+}
